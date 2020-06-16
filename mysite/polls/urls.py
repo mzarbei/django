39edc18406afba9fd738efbe2e5933b128d52aa3
mzz <mzarbei@gmail.com>
2020-06-16 12:06:30 +0330

    modifying polls/urls.py for general views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from .           import views
 app_name='polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:pk>/details/', views.details, name="details"),
-    path('<int:pk>/results/', views.results, name='results'),
+    path('', views.IndexView.as_view, name='index'),
+    path('<int:pk>/details/', views.DetailView.as_view, name="details"),
+    path('<int:pkgit >/results/', views.ResultsView.as_view, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
