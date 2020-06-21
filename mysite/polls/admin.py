7e747329f2461b25ab6cbe7192a338d2130a2f4c
mzz <mzarbei@gmail.com>
2020-06-21 13:42:34 +0330

    adding polls/admin new version and polls/index.html

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Question, Choice
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pu_date', 'question_text']
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 
 # Register your models here.
